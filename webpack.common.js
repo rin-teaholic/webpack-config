71ac13bcd278ba58af5d2cc4daee396760e7c1fa
Rin <info@rin-m.site>
2021-08-02 09:46:54 +0900

    webpack5 basic config

---
+++ webpack.common.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+    assetModuleFilename: "images/[contenthash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html/,
+        loader: "html-loader",
+        // type: 'asset/resource',
+        generator: {
+          filename: '[name][ext][query]'
+        }
+      }
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+
+  devtool: "source-map",
+};
